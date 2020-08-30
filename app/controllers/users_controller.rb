ee8c0cfd1cdd34f39a3a4217056edc351952acec
ktakizawa <tmatsuda4recruit@gmail.com>
2020-08-30 16:56:22 +0900

    サインアップフォーでユーザー登録が可能に

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
@@ -19,7 +21,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :password,
+    params.require(:user).permit(:name, :email, :password,
                     :password_confirmation)
   end
 end
